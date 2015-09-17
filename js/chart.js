355c0c1ffab30c7dc341828702c2693b998e500e
Louis Dionne <ldionne.2@gmail.com>
2015-09-16 20:28:50 -0400

    Update chart.js to match the one used by Hana

--- js/chart.js
+++ js/chart.js
@@ -23,9 +23,17 @@ Distributed under the Boost Software License, Version 1.0.
       };
     }
 
+    if (options.subtitle == undefined) {
+      options.subtitle = { text: "(smaller is better)" };
+    }
+
     if (options.chart == undefined) {
       options.chart = { zoomType: 'xy' };
     }
+
+    options.plotOptions = options.plotOptions || {};
+    options.plotOptions.series = options.plotOptions.series || {};
+    options.plotOptions.series.marker = options.plotOptions.series.marker || { enabled: false };
 
     if (options.title.x == undefined) {
       options.title.x = -20; // center
@@ -34,6 +42,25 @@ Distributed under the Boost Software License, Version 1.0.
     if (options.series.stickyTracking == undefined) {
       options.series.stickyTracking = false;
     }
+
+    // Fix the colors so that a series has the same color on all the charts.
+    // Based on the colors at http://api.highcharts.com/highcharts#colors.
+    var colorMap = {
+        'hana::tuple': '#f45b5b'
+      , 'hana::basic_tuple': '#434348'
+      , 'mpl::vector': '#90ed7d'
+      , 'std::array': '#8085e9'
+      , 'fusion::vector': '#f7a35c'
+      , 'std::vector: ': '#f15c80'
+      , 'std::tuple': '#e4d354'
+      , 'hana::set': '#2b908f'
+      , 'hana::map': '#7cb5ec'
+      , 'fusion::list': '#91e8e1'
+    };
+    options.series.forEach(function(series) {
+      if (colorMap[series.name])
+        series.color = colorMap[series.name];
+    });
 
     options.tooltip = options.tooltip || {};
     options.tooltip.valueSuffix = options.tooltip.valueSuffix || 's';
