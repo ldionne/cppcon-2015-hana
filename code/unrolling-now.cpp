3bf770f57ea0462542e4a21f0e8d00f087a203a0
Louis Dionne <ldionne.2@gmail.com>
2015-09-17 15:54:57 -0400

    Squeeze the assembly output for unrolling

--- code/unrolling-now.cpp
+++ code/unrolling-now.cpp
@@ -4,11 +4,14 @@
 #include <boost/hana/integral_constant.hpp>
 using namespace boost::hana;
 
+// To generate, first do not define f(), i.e. remove the trailing `{ }`.
+// Otherwise, the compiler optimizes everything away. Then,
+// clang++-3.6 -S -O3 -c code/unrolling-now.cpp -I path/to/hana/include -std=c++14 -o unrolling-now.s
 
 // sample(unrolling-now)
 __attribute__((noinline)) void f() { }
 
 int main() {
-    int_c<10>.times(f);
+    int_c<5>.times(f);
 }
 // end-sample
